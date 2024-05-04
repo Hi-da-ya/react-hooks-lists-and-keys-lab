c28247a1bfa4f654cb8e186fa65587e00443f99c
Hidaya Wetsakhulia <hidayawetsa@gmail.com>
2024-05-04 17:23:03 +0300

    lab complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const navItems = links.map((item) => (
+      <a key={item} href={`#${item}?key=value` } >{item}</a>
+  ))
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */}
+ {navItems}
+  
+  </nav>;
 }
 
 export default NavBar;
